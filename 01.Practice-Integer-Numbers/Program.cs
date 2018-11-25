449a40778c3291b54d7e97c33d5a173c02e78203
KalinVasilevSV <kalinsv@abv.bg>
2018-11-25 20:36:00 +0200

    First commit

---
+++ 01.Practice-Integer-Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Practice_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
