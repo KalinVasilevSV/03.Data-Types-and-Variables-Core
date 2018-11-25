fc833769115d9109b04d3c338f500578a82a21b6
KalinVasilevSV <kalinsv@abv.bg>
2018-11-26 01:35:41 +0200

    Evening work on 25.11

--- 01.Practice-Integer-Numbers/Program.cs
+++ 01.Practice-Integer-Numbers/Program.cs
@@ -6,7 +6,21 @@ namespace _01.Practice_Integer_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            sbyte n1 = sbyte.Parse(Console.ReadLine());
+            byte n2 = byte.Parse(Console.ReadLine());
+            short n3 = short.Parse(Console.ReadLine());
+            ushort n4 = ushort.Parse(Console.ReadLine());
+            uint n5 = uint.Parse(Console.ReadLine());
+            int n6 = int.Parse(Console.ReadLine());
+            long n7 = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(n7);
         }
     }
 }
---
+++ 02.Practice-Floating-Points/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.Practice_Floating_Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n1 = decimal.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            decimal n3 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+        }
+    }
+}
+ 
