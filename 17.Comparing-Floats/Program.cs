f6ad7df86c1a1974335639923316dba262c86d6c
KalinVasilevSV <kalinsv@abv.bg>
2018-11-27 13:07:08 +0200

    Morning work on 27.11

--- 17.Comparing-Floats/Program.cs
+++ 17.Comparing-Floats/Program.cs
@@ -6,20 +6,30 @@ namespace _17.Comparing_Floats
     {
         static void Main(string[] args)
         {
-            float firstNum = float.Parse(Console.ReadLine());
-            float secondNum = float.Parse(Console.ReadLine());
+            //Type double is better than float
+            //Float causes anomalies due to insufficient precision
+
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
 
             float eps = 0.000001f;
+            bool areEqual = false;
+
+            //Math.Round() works as good as the Convert approach
+            //Both of the methods can't compate very small numbers with opposite sign but work fine with the same sign
 
             //firstNum = (float)Math.Round(firstNum, 6);
             //secondNum = (float)Math.Round(secondNum, 6);
 
-            //Math.Round() works as good as the Convert approach
+            //firstNum = (float)Convert.ToDouble($"{Convert.ToString(firstNum):0.000000}");
+            //secondNum = (float)Convert.ToDouble($"{Convert.ToString(secondNum):0.000000}");
 
-            firstNum = (float)Convert.ToDouble($"{Convert.ToString(firstNum):0.000000}");
-            secondNum = (float)Convert.ToDouble($"{Convert.ToString(secondNum):0.000000}");
+            //areEqual=(firstNum==secondNum);
 
-            Console.WriteLine(firstNum==secondNum);
+            double difference = Math.Abs(firstNum - secondNum);
+            if (difference < eps) areEqual = true;
+
+            Console.WriteLine(areEqual);
         }
     }
 }
---
+++ 18.Print-Part-of-the-ASCII-Table/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _18.Print_Part_of_the_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
