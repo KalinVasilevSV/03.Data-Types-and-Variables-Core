71a7d25589fad78dce57813a73566007106057bb
KalinVasilevSV <kalinsv@abv.bg>
2018-11-27 02:14:48 +0200

    Evening work on 26.11 late

---
+++ 15.Integer-to-Hex-and-Binary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _15.Integer_to_Hex_and_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inDecimal = int.Parse(Console.ReadLine());
+
+            string hex = Convert.ToString(inDecimal, 16).ToUpper();
+            string binary = Convert.ToString(inDecimal, 2);
+
+            Console.WriteLine(hex);
+            Console.WriteLine(binary);
+        }
+    }
+}
---
+++ 17.Comparing-Floats/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _17.Comparing_Floats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float firstNum = float.Parse(Console.ReadLine());
+            float secondNum = float.Parse(Console.ReadLine());
+
+            float eps = 0.000001f;
+
+            //firstNum = (float)Math.Round(firstNum, 6);
+            //secondNum = (float)Math.Round(secondNum, 6);
+
+            //Math.Round() works as good as the Convert approach
+
+            firstNum = (float)Convert.ToDouble($"{Convert.ToString(firstNum):0.000000}");
+            secondNum = (float)Convert.ToDouble($"{Convert.ToString(secondNum):0.000000}");
+
+            Console.WriteLine(firstNum==secondNum);
+        }
+    }
+}
